e84ab6fd24089ae8a05b62dd0ece68c9f5c03ae6
usmany26 <usmanyousaf71551@gmail.com>
2023-05-26 06:51:52 +0500

    fds

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,45 @@
 export const projects = [
   {
+    title: "Ai-Summz",
+    description:
+      "Using React, RTK, RapidApi , Improve your reading with AiSummz, an open-source article summarizer that change extensive articles into clear and compact outlines.",
+    image: "/images/aisummz.png",
+    tags: ["React", "TRK", "Tailwind", "RapiApi"],
+    visit: "https://github.com/usmany26/ai-summz",
+    source: "https://ai-ar-summarizer.netlify.app/",
+    id: 0,
+  },
+  {
+    title: "Justtap",
+    description:
+      "A just-tap device allows you to seamlessly transfer all of your information on your Just-tap Profile with a single tap onto someone else's smartphone. Your Just-tap Profile is fully customizable -Including your contact information, social media links, payment info, and more- and enables you to share every important detail about you and your business with your network. ",
+    image: "/images/justtap.png",
+    tags: ["React.js", "Firebase", "Redux", "Redux Toolkit"],
+    source: "https://shop.justtap.us",
+    visit: "https://shop.justtap.us",
+    id: 2,
+  },
+  {
+    title: "Utube",
+    description:
+      "Utube ia lightweight version of youtube. Buy using RapisApi youtube 3 apis to get youtube videos, channels atc.",
+    image: "/images/utube.png",
+    tags: ["React.js", "Material Ui"],
+    visit: "https://github.com/usmany26/utube",
+    source: "https://utube-git-main-usmany26.vercel.app/",
+    id: 3,
+  },
+  {
+    title: "Sushiman",
+    description:
+      "A medern design Sushi food restaurant app. Showcasing the food with animations",
+    image: "/images/sushi.png",
+    tags: ["Html", "CSS"],
+    visit: "https://github.com/usmany26/sushi",
+    source: "https://sushi-kbyppqslo-usmany26.vercel.app/#food",
+    id: 1,
+  },
+  {
     title: "Metanation",
     description:
       "A next js article sharing website with the latest article and news about metaverse, crypto, NFT's etc",
@@ -17,26 +57,6 @@ export const projects = [
     source: "https://metanation.uk/",
     visit: "https://metanation.uk/",
     id: 1,
-  },
-  {
-    title: "Ai-Summz",
-    description:
-      "Using React, RTK, RapidApi , Improve your reading with AiSummz, an open-source article summarizer that change extensive articles into clear and compact outlines.",
-    image: "/images/aisummz.png",
-    tags: ["React", "TRK", "Tailwind", "RapiApi"],
-    source: "https://ai-ar-summarizer.netlify.app/",
-    visit: "https://ai-ar-summarizer.netlify.app/",
-    id: 0,
-  },
-  {
-    title: "Justtap",
-    description:
-      "A just-tap device allows you to seamlessly transfer all of your information on your Just-tap Profile with a single tap onto someone else's smartphone. Your Just-tap Profile is fully customizable -Including your contact information, social media links, payment info, and more- and enables you to share every important detail about you and your business with your network. ",
-    image: "/images/justtap.png",
-    tags: ["React.js", "Firebase", "Redux", "Redux Toolkit"],
-    source: "https://shop.justtap.us",
-    visit: "https://shop.justtap.us",
-    id: 2,
   },
 ];
 
